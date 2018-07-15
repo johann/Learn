c7fe6e5fb36f4e04e98c7cfa4b4e63a778b50118
Johann Kerr <johannkerr@gmail.com>
2018-07-15 12:46:56 -0400

    Initial Commit

---
+++ Learn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learn
+//
+//  Created by Johann Kerr on 7/15/18.
+//  Copyright © 2018 Johann Kerr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
