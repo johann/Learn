987885ed3b41cc9bd9ab5eb40d6a5ffe65fe090f
lukeghenco <lukeghenco@gmail.com>
2019-04-02 20:31:29 -0700

    Add Batch, Course & Track models for decoding JSON.
    This will allow a User to have the additional coded fields as well.

---
+++ Learn/models/Batch.swift
@@ -0,0 +1,16 @@
+//
+//  Batch.swift
+//  Learn
+//
+//  Created by Luke Ghenco on 4/2/19.
+//  Copyright © 2019 Johann Kerr. All rights reserved.
+//
+
+import Foundation
+
+struct Batch: Codable {
+    var id: Int
+    var iteration: String
+    var uuid: String
+    var displayName: String
+}
---
+++ Learn/models/Course.swift
@@ -0,0 +1,16 @@
+//
+//  Course.swift
+//  Learn
+//
+//  Created by Luke Ghenco on 4/2/19.
+//  Copyright © 2019 Johann Kerr. All rights reserved.
+//
+
+import Foundation
+
+struct Course: Codable {
+    var id: Int
+    var displayName: String
+    var slug: String
+    var uuid: String
+}
