e0f0b0c43fbdc354453cdfb9db37579cc2e81f8c
lukeghenco <lukeghenco@gmail.com>
2019-04-04 04:48:52 -0700

    Clean up Constant variables to fit with Endpoint specifications

--- Learn/utilities/Constants.swift
+++ Learn/utilities/Constants.swift
@@ -10,8 +10,8 @@ import Foundation
 
 
 struct Constants {
-    static let qaLearnHost = "qa-ironboard04.fe.flatironschool.com/api/"
-    static let localLearnAPI = "https://ea166a80.ngrok.io"
+    static let qaLearnHost = "qa-ironboard04.fe.flatironschool.com"
+    static let localHost = "85fafdb3.ngrok.io"
     static let consumerKey = "c00d880168abfa5409b69c9267989a60019d5757fcf5095c1bdc16513f48dd49"
     static let consumerSecret = "726ed128be992068be3ea6579d5ef49c47eb78dc82366ff20dc1fc89c472bef2"
 }
