0c5781e3e104e7495c6d478642db2e00610efd0f
Johann Kerr <johannkerr@gmail.com>
2019-04-04 09:56:01 -0400

    add coredata

--- Learn/stores/UserDataStore.swift
+++ Learn/stores/UserDataStore.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreData
 
 
 final class UserDataStore {
@@ -14,7 +15,6 @@ final class UserDataStore {
     var student: Student?
     
     fileprivate init() {}
-    
     func fetchProfile(completion: @escaping (Student) -> ()) {
         LearnApi.getProfile() { (student) in
             self.student = student
@@ -27,6 +27,30 @@ final class UserDataStore {
         LearnApi.getCanonicalProgress(batch: "597", track: "25054") { (student) in
             self.student = student
             completion(student)
+        }
+    }
+    
+    // MARK: Persistence Layer
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Learn")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    private func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
         }
     }
     
