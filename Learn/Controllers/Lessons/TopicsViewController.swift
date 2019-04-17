59323a8b21853ad23b9f50dd33b805cb306def98
lukeghenco <lukeghenco@gmail.com>
2019-04-17 15:48:16 -0700

    Add TopiclCollectionViewCell that renders topic titles

--- Learn/Controllers/Lessons/TopicsViewController.swift
+++ Learn/Controllers/Lessons/TopicsViewController.swift
@@ -10,41 +10,50 @@ import UIKit
 
 class TopicsViewController: UICollectionViewController {
     let store = UserDataStore.shared
-    var topics: [Topic] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let _ = store.student {
             store.fetchCurriculum { (track) in
-                self.updateViewWith(track)
+                self.reloadView()
             }
         } else {
             store.fetchProfile { (_) in
                 self.store.fetchCurriculum { (track) in
-                    self.updateViewWith(track)
+                    self.reloadView()
                 }
             }
         }
     }
     
     // MARK: Update Track Info
-    func updateViewWith(_ track: Track) {
-        guard let topics = track.topics else { return }
-        self.topics = topics
+    func reloadView() {
+        DispatchQueue.main.async {
+            self.collectionView.reloadSections(IndexSet(integer: 0))
+        }
     }
 
     // MARK: - Navigation
 
     // MARK: UICollectionViewDataSource
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.topics.count
+        guard let track = self.store.track else { return 0 }
+        guard let topics = track.topics else { return 0 }
+        
+        return topics.count
     }
 
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "topicCell", for: indexPath)
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> TopicCollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "topicCell", for: indexPath) as! TopicCollectionViewCell
+        
+        guard let track = self.store.track else { return cell }
+        guard let topics = track.topics else { return cell }
+        
+        let topic = topics[indexPath.row]
     
         cell.backgroundColor = UIColor.random
+        cell.topicTitleField.text = topic.title
         return cell
     }
 
