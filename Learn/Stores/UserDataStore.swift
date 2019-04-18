96b872efe05005ff0bc04cdf6a7abac0d10bd789
Johann Kerr <johannkerr@gmail.com>
2019-04-18 14:24:41 -0400

    update datastore

--- Learn/Stores/UserDataStore.swift
+++ Learn/Stores/UserDataStore.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreData
 
 final class UserDataStore {
     static let shared = UserDataStore()
@@ -37,8 +38,7 @@ final class UserDataStore {
         guard let student = self.student else { fatalError("Student is not defined") }
         let userId = student.id
         let batchId = student.activeBatch.id
-//        let trackId = student.activeTrack.id
-        let trackId = 45971
+        let trackId = student.activeTrack.id
         
         LearnApi().getCurriculum(token, userId: userId, batchId: batchId, trackId: trackId) { (response) in
             switch response {
@@ -51,6 +51,30 @@ final class UserDataStore {
                 break
             }
             
+        }
+    }
+    
+    // MARK: Persistence Layer
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Learn")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    private func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
         }
     }
 }
