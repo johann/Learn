2a6a111bb5e5c606dcbb7acb8bbc2abcded6a802
lukeghenco <lukeghenco@gmail.com>
2019-05-14 08:46:25 -0400

    Add success and failure tests for LearnAPI

--- LearnTests/Adapters/LearnApiTest.swift
+++ LearnTests/Adapters/LearnApiTest.swift
@@ -10,6 +10,19 @@ import XCTest
 
 @testable import Learn
 
+class TestJSON {
+    let data: Data?
+    let decoder: JSONDecoder
+    
+    init(_ fileName: String) {
+        let bundle = Bundle(for: type(of: self))
+        let url = bundle.url(forResource: fileName, withExtension: "json")
+        self.data = try? Data(contentsOf: url!)
+        self.decoder = JSONDecoder()
+        self.decoder.keyDecodingStrategy = .convertFromSnakeCase
+    }
+}
+
 class LearnApiTest: XCTestCase {
     
     override func setUp() {}
@@ -20,9 +33,11 @@ class LearnApiTest: XCTestCase {
     // MARK: - On Success
     func test_LearnAPI_GetProfile_ReturnsStudent() {
         class WebServiceMock: WebService {
-            
             override func request(_ endPoint: Endpoint, headers: [String: String], method: String = "GET", result: @escaping (Result<Data, Error>) -> Void) {
-                let data = Data(AdaptersTestData.successfulLearnAPIProfileJSON.utf8)
+                guard let data = TestJSON("profileFetchData").data else {
+                    XCTFail()
+                    return
+                }
                 result(.success(data))
             }
         }
@@ -30,34 +45,36 @@ class LearnApiTest: XCTestCase {
         LearnApi(WebServiceMock()).getProfile("abc") { (result) in
             switch result {
             case .success(let student):
+                let testJSON = TestJSON("profileFetchData")
+                let studentStub = try! testJSON.decoder.decode(Student.self, from: testJSON.data!)
+                let trackStub = studentStub.activeTrack
+                let batchStub = studentStub.activeBatch
+                let courseStub = studentStub.activeCourse
                 let activeTrack = student.activeTrack
                 let activeBatch = student.activeBatch
                 let activeCourse = student.activeCourse
                 
-                XCTAssertTrue(student.gravatarUrl == "https://avatars.githubusercontent.com/u/123456", "The Student's gravatarURL should be: https://avatars.githubusercontent.com/u/123456")
-                XCTAssertTrue(student.learnUuid == "111-222-333", "The Student's active learnUuid should be: 111-222-333")
-                XCTAssertTrue(student.displayName == "Flint Lockwood", "The Student's displayName should be: Flint Lockwood")
-                XCTAssertTrue(student.totalLessonsCount == 4, "The Student's totalLessonsCount should be: 4")
-                XCTAssertTrue(student.email == "test@gmail.com", "The Student's email should be: test@gmail.com")
-                XCTAssertTrue(student.id == 26, "The Student's email should be 26")
-                XCTAssertTrue(student.velocity == 0, "The Students's velocity should be 0")
-                XCTAssertTrue(student.lastSeenAt == "2019-03-12 01:18:01", "The Student's result should be 2019-03-12 01:18:01")
-                XCTAssertTrue(student.learnUsername == "steadfast-delimiter-7257", "The Student's learnUsername should be: steadfast-delimiter-7257")
-                
-                XCTAssertTrue(activeTrack.id == 1, "The Student's active track id should be: 1")
-                XCTAssertTrue(activeTrack.slug == "intro-to-data-science", "The Student's active track slag should be: intro-to-data-science")
-                XCTAssertTrue(activeTrack.title == "Intro to Data Science", "The Student's active track title should be: Intro to Data Science")
-                XCTAssertTrue(activeTrack.uuid == "aaa-bbb-ccc", "The Student's active track uuid should be: aaa-bbb-ccc")
-                
-                XCTAssertTrue(activeBatch.id == 11, "The Student's active batch id should be: 11")
-                XCTAssertTrue(activeBatch.iteration == "online-ds-sp-000", "The Student's active batch iteration should be: online-ds-sp-000")
-                XCTAssertTrue(activeBatch.displayName == "Online DS SP-000", "The Student's active batch displayName should be: Online DS SP-000")
-                XCTAssertTrue(activeBatch.uuid == "111-aaa-222", "The Student's active batch uuid should be: 111-aaa-222")
-                
-                XCTAssertTrue(activeCourse.id == 15, "The Student's activeCourse.id should be: 11")
-                XCTAssertTrue(activeCourse.slug == "online-data-science-bootcamp", "The Student's activeCourse.iteration should be: online-data-science-bootcamp")
-                XCTAssertTrue(activeCourse.displayName == "Online Data Science Bootcamp", "The Student's activeCourse.displayName should be: Online Data Science Bootcamp")
-                XCTAssertTrue(activeCourse.uuid == "aaa-111-bbb", "The Student's activeCourse.uuid should be: aaa-111-bbb")
+                XCTAssert(student.gravatarUrl == studentStub.gravatarUrl, "student.gravatarURL should be: \(String(describing: studentStub.gravatarUrl))")
+                XCTAssert(student.learnUuid == studentStub.learnUuid, "student.learnUuid should be: \(studentStub.learnUuid)")
+                XCTAssert(student.displayName == studentStub.displayName, "student.displayName should be: \(studentStub.displayName)")
+                XCTAssert(student.totalLessonsCount == studentStub.totalLessonsCount, "student.totalLessonsCount should be: \(studentStub.totalLessonsCount)")
+                XCTAssert(student.email == studentStub.email, "student.email should be: \(studentStub.email)")
+                XCTAssert(student.id == studentStub.id, "student.id should be: \(studentStub.id)")
+                XCTAssert(student.velocity == studentStub.velocity, "student.velocity should be: \(studentStub.velocity)")
+                XCTAssert(student.lastSeenAt == studentStub.lastSeenAt, "student.lastSeenAt should be: \(String(describing: studentStub.lastSeenAt))")
+                XCTAssert(student.learnUsername == studentStub.learnUsername, "student.learnUsername should be: \(studentStub.learnUsername)")
+                XCTAssert(activeTrack.id == trackStub.id, "activeTrack.id should be: \(trackStub.id)")
+                XCTAssert(activeTrack.slug == trackStub.slug, "activeTrack.slag should be: \(trackStub.slug)")
+                XCTAssert(activeTrack.title == trackStub.title, "activeTrack.title should be: \(trackStub.title)")
+                XCTAssert(activeTrack.uuid == trackStub.uuid, "activeTrack.uuid should be: \(trackStub.uuid)")
+                XCTAssert(activeBatch.id == batchStub.id, "activeBatch.id should be: \(batchStub.id)")
+                XCTAssert(activeBatch.iteration == batchStub.iteration, "activeBatch.iteration should be: \(batchStub.iteration)")
+                XCTAssert(activeBatch.displayName == batchStub.displayName, "activeBatch.displayName should be: \(batchStub.displayName)")
+                XCTAssert(activeBatch.uuid == batchStub.uuid, "activeBatch.uuid should be: \(batchStub.uuid)")
+                XCTAssert(activeCourse.id == courseStub.id, "activeCourse.id should be: \(String(describing: courseStub.id))")
+                XCTAssert(activeCourse.slug == courseStub.slug, "activeCourse.iteration should be: \(String(describing: courseStub.slug))")
+                XCTAssert(activeCourse.displayName == courseStub.displayName, "activeCourse.displayName should be: \(String(describing: courseStub.displayName))")
+                XCTAssert(activeCourse.uuid == "aaa-111-bbb", "activeCourse.uuid should be: aaa-111-bbb")
             default:
                 XCTFail()
             }
@@ -68,8 +85,10 @@ class LearnApiTest: XCTestCase {
     func test_LearnAPI_GetProfileWithMalformedJSON_ReturnsNetworkError() {
         class WebServiceMock: WebService {
             override func request(_ endPoint: Endpoint, headers: [String: String], method: String = "GET", result: @escaping (Result<Data, Error>) -> Void) {
-                let data = Data(AdaptersTestData.unsuccessfulLearnAPIProfileJSON.utf8)
-                print(data)
+                guard let data = TestJSON("emptyResponse").data else {
+                    XCTFail()
+                    return
+                }
                 result(.success(data))
             }
         }
@@ -80,7 +99,7 @@ class LearnApiTest: XCTestCase {
                 XCTFail()
             case .failure(let error):
                 let networkError = error as! NetworkError
-                XCTAssertTrue(networkError == NetworkError.malformedJSON, "Should return NetworkError.malformedJSON")
+                XCTAssert(networkError == NetworkError.malformedJSON, "Should return NetworkError.malformedJSON")
             }
         }
     }
@@ -101,7 +120,7 @@ class LearnApiTest: XCTestCase {
                 XCTFail()
             case .failure(let error):
                 let networkError = error as! NetworkError
-                XCTAssertTrue(networkError == NetworkError.urlFailure, "Should return NetworkError.urlFailure")
+                XCTAssert(networkError == NetworkError.urlFailure, "Should return NetworkError.urlFailure")
             }
         }
     }
@@ -120,7 +139,7 @@ class LearnApiTest: XCTestCase {
                 XCTFail()
             case .failure(let error):
                 let networkError = error as! NetworkError
-                XCTAssertTrue(networkError == NetworkError.missingData, "Should return NetworkError.missingData")
+                XCTAssert(networkError == NetworkError.missingData, "Should return NetworkError.missingData")
             }
         }
     }
@@ -129,9 +148,11 @@ class LearnApiTest: XCTestCase {
     // MARK: - On Success
     func test_LearnAPI_GetCurriculum_ReturnsTrack() {
         class WebServiceMock: WebService {
-            
             override func request(_ endPoint: Endpoint, headers: [String: String], method: String = "GET", result: @escaping (Result<Data, Error>) -> Void) {
-                let data = Data(AdaptersTestData.successfulLearnAPITrackJSON.utf8)
+                guard let data = TestJSON("curriculumFetchData").data else {
+                    XCTFail()
+                    return
+                }
                 result(.success(data))
             }
         }
@@ -139,18 +160,92 @@ class LearnApiTest: XCTestCase {
         LearnApi(WebServiceMock()).getCurriculum("abc", userId: 26, batchId: 11, trackId: 1) { (result) in
             switch result {
             case .success(let track):
-//                let topic = track.topics?[0]
-//                let unit = topic.units?[0]
-//                let lesson = unit.lessons?[0]
+                let testJSON = TestJSON("curriculumFetchData")
+                let trackStub = try! testJSON.decoder.decode(Track.self, from: testJSON.data!)
+                let topicStub = trackStub.topics![0]
+                let unitStub = topicStub.units![0]
+                let lessonStub = unitStub.lessons![0]
+                let topic = track.topics![0]
+                let unit = topic.units![0]
+                let lesson = unit.lessons![0]
                 
-                XCTAssertTrue(track.id == 1, "The Track's id should be: 1")
-                XCTAssertTrue(track.slug == "intro-to-data-science", "The Track's slug should be: intro-to-data-science")
-                XCTAssertTrue(track.title == "Intro to Data Science", "The Tracks's title should be: Intro to Data Science")
-                XCTAssertTrue(track.uuid == "111-222-333", "The Tracks's uuid should be: 111-222-333")
-                // TODO: Add additional tests
-                
+                XCTAssert(track.id == trackStub.id, "The Track's id should be: \(trackStub.id)")
+                XCTAssert(track.slug == trackStub.slug, "The Track's slug should be: \(trackStub.slug)")
+                XCTAssert(track.title == trackStub.title, "The Tracks's title should be: \(trackStub.title)")
+                XCTAssert(track.uuid == trackStub.uuid, "The Tracks's uuid should be: \(trackStub.uuid)")
+                XCTAssert(topic.id == topicStub.id, "The topic.id should be: \(topicStub.id)")
+                XCTAssert(topic.slug == topicStub.slug, "The topic.slug should be \(topicStub.slug)")
+                XCTAssert(topic.title == topicStub.title, "The topic.title should be: \(topicStub.title)")
+                XCTAssert(unit.id == unitStub.id, "The unit.id should be: \(unitStub.id)")
+                XCTAssert(unit.title == unitStub.title, "The unit.title should be: \(unitStub.title)")
+                XCTAssert(unit.slug == unitStub.slug, "The unit.slug should be: \(unitStub.slug)")
+                XCTAssert(lesson.id == lessonStub.id, "The lesson.id should be: \(lessonStub.id)")
+                XCTAssert(lesson.readme == lessonStub.readme, "The lesson.readme should be: \(lessonStub.readme)")
+                XCTAssert(lesson.slug == lessonStub.slug, "The lesson.slug should be: \(lessonStub.slug)")
+                XCTAssert(lesson.title == lessonStub.title, "The lesson.title should be: \(lessonStub.title)")
             default:
                 XCTFail()
+            }
+        }
+    }
+    
+    // MARK: - On Failure
+    func test_LearnAPI_GetCurriculumWithMalformedJSON_ReturnsNetworkError() {
+        class WebServiceMock: WebService {
+            override func request(_ endPoint: Endpoint, headers: [String: String], method: String = "GET", result: @escaping (Result<Data, Error>) -> Void) {
+                guard let data = TestJSON("emptyResponse").data else {
+                    XCTFail()
+                    return
+                }
+                result(.success(data))
+            }
+        }
+        
+        LearnApi(WebServiceMock()).getCurriculum("abc", userId: 26, batchId: 11, trackId: 1) { (result) in
+            switch result {
+            case .success(_):
+                XCTFail()
+            case .failure(let error):
+                let networkError = error as! NetworkError
+                XCTAssert(networkError == NetworkError.malformedJSON, "Should return NetworkError.malformedJSON")
+            }
+        }
+    }
+    
+    func test_LearnAPI_GetCurriculumWithBadURL_ReturnsNetWorkError() {
+        class WebServiceMock: WebService {
+            
+            override func request(_ endPoint: Endpoint, headers: [String: String], method: String = "GET", result: @escaping (Result<Data, Error>) -> Void) {
+                result(.failure(NetworkError.urlFailure))
+            }
+        }
+        
+        LearnApi(WebServiceMock()).getCurriculum("abc", userId: 26, batchId: 11, trackId: 1)  { (result) in
+            switch result {
+            case .success(_):
+                XCTFail()
+            case .failure(let error):
+                let networkError = error as! NetworkError
+                XCTAssert(networkError == NetworkError.urlFailure, "Should return NetworkError.urlFailure")
+            }
+        }
+    }
+    
+    func test_LearnAPI_GetCurriculumWithMissingData_ReturnsNetworkError() {
+        class WebServiceMock: WebService {
+            
+            override func request(_ endPoint: Endpoint, headers: [String: String], method: String = "GET", result: @escaping (Result<Data, Error>) -> Void) {
+                result(.failure(NetworkError.missingData))
+            }
+        }
+        
+        LearnApi(WebServiceMock()).getCurriculum("abc", userId: 26, batchId: 11, trackId: 1)  { (result) in
+            switch result {
+            case .success(_):
+                XCTFail()
+            case .failure(let error):
+                let networkError = error as! NetworkError
+                XCTAssert(networkError == NetworkError.missingData, "Should return NetworkError.missingData")
             }
         }
     }
