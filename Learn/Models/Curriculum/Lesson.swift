4616855beb467ce57477255ee8da0a8d0263ba2a
lukeghenco <lukeghenco@gmail.com>
2019-05-15 13:16:09 -0700

    Add ".isComplete" to Lesson model

--- Learn/Models/Curriculum/Lesson.swift
+++ Learn/Models/Curriculum/Lesson.swift
@@ -13,4 +13,22 @@ struct Lesson: Curriculum, Codable {
     var slug: String
     var title: String
     var readme: String
+    var isComplete: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case slug = "slug"
+        case title = "title"
+        case readme = "readme"
+        case isComplete = "isComplete"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(Int.self, forKey: .id)
+        slug = try values.decode(String.self, forKey: .slug)
+        title = try values.decode(String.self, forKey: .title)
+        readme = try values.decode(String.self, forKey: .readme)
+        isComplete = try values.decodeIfPresent(Bool.self, forKey: .isComplete) ?? false
+    }
 }
---
+++ LearnTests/Models/Curriculum/LessonTest.swift
@@ -0,0 +1,104 @@
+//
+//  LessonTest.swift
+//  LearnTests
+//
+//  Created by Luke Ghenco on 5/15/19.
+//  Copyright © 2019 Johann Kerr. All rights reserved.
+//
+
+import XCTest
+@testable import Learn
+
+final class LessonTest: XCTestCase {
+
+    override func setUp() {}
+
+    override func tearDown() {}
+    
+    func test_LessonInit_WithMissingId_ThrowsError() {
+        XCTAssertThrowsError(try JSONDecoder().decode(Lesson.self, from: lessonDataMissingId)) { error in
+            if case .keyNotFound(let key, _)? = error as? DecodingError {
+                XCTAssertEqual("id", key.stringValue)
+            } else {
+                XCTFail("Expected '.keyNotFound' for 'id' but error was: \(error)")
+            }
+        }
+    }
+    
+    func test_LessonInit_WithMissingSlug_ThrowsError() {
+        XCTAssertThrowsError(try JSONDecoder().decode(Lesson.self, from: lessonDataMissingSlug)) { error in
+            if case .keyNotFound(let key, _)? = error as? DecodingError {
+                XCTAssertEqual("slug", key.stringValue)
+            } else {
+                XCTFail("Expected '.keyNotFound' for 'slug' but error was: \(error)")
+            }
+        }
+    }
+    
+    func test_LessonInit_WithMissingTitle_ThrowsError() {
+        XCTAssertThrowsError(try JSONDecoder().decode(Lesson.self, from: lessonDataMissingTitle)) { error in
+            if case .keyNotFound(let key, _)? = error as? DecodingError {
+                XCTAssertEqual("title", key.stringValue)
+            } else {
+                XCTFail("Expected '.keyNotFound' for 'title' but error was: \(error)")
+            }
+        }
+    }
+    
+    func test_LessonInit_WithMissingReadmeAttr_ThrowsError() {
+        XCTAssertThrowsError(try JSONDecoder().decode(Lesson.self, from: lessonDataMissingReadme)) { error in
+            if case .keyNotFound(let key, _)? = error as? DecodingError {
+                XCTAssertEqual("readme", key.stringValue)
+            } else {
+                XCTFail("Expected '.keyNotFound' for 'readme' but error was: \(error)")
+            }
+        }
+    }
+    
+    func test_LessonInit_WithMissingIsCompleteAttr_SetsISCompleteAsFalse() throws {
+        let sut = try JSONDecoder().decode(Lesson.self, from: lessonDataMissingIsComplete)
+        
+        XCTAssert(sut.isComplete == false, "Lesson '.isComplete' should be 'true' when not defined")
+    }
+}
+
+private let lessonDataMissingIsComplete = Data("""
+{
+    "id": 1,
+    "slug": "functions",
+    "title": "Functions",
+    "readme": "test readme file"
+}
+""".utf8)
+
+private let lessonDataMissingId = Data("""
+{
+    "slug": "functions",
+    "title": "Functions",
+    "readme": "test readme file"
+}
+""".utf8)
+
+private let lessonDataMissingSlug = Data("""
+{
+    "id": 1,
+    "title": "Functions",
+    "readme": "test readme file"
+}
+""".utf8)
+
+private let lessonDataMissingTitle = Data("""
+{
+    "id": 1,
+    "slug": "functions",
+    "readme": "test readme file"
+}
+""".utf8)
+
+private let lessonDataMissingReadme = Data("""
+{
+    "id": 1,
+    "slug": "functions",
+    "title": "Functions"
+}
+""".utf8)
