3acdbb90f680bd5af6b307c86f2dbd9bffc3363e
Johann Kerr <johannkerr@gmail.com>
2019-05-16 11:56:48 -0400

    add to lessonview

--- Learn/Activities/Curiculum/CurriculumCoordinator.swift
+++ Learn/Activities/Curiculum/CurriculumCoordinator.swift
@@ -14,10 +14,7 @@ class CurriculumCoordinator: Coordinator {
     init(navigationController: UINavigationController = .init()) {
         self.navigationController = navigationController
 //        self.navigationController.navigationBar.prefersLargeTitles = true
-        
-//        let topicVC = TopicsViewController.instantiate()
-//        topicVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "literature"), tag: 0)
-//        topicVC.coordinator = self
+
     
         let topicVC = TopicViewController.instantiate()
         topicVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "literature"), tag: 0)
@@ -41,7 +38,22 @@ class CurriculumCoordinator: Coordinator {
         lessonVC.configureLesson(lesson)
         let navigationController = UINavigationController()
         navigationController.viewControllers = [lessonVC]
+        let closeBtn = UIBarButtonItem(image: UIImage(named: "close_icon"), style: .plain, target: self, action: #selector(close))
+        let saveBtn = UIBarButtonItem(image: UIImage(named: "bookmark_saved"), style: .plain, target: self, action: #selector(save))
+        navigationController.navigationBar.tintColor = UIColor.black
+        
+        lessonVC.navigationItem.leftBarButtonItem = closeBtn
+        lessonVC.navigationItem.rightBarButtonItem = saveBtn
+
         self.navigationController.present(navigationController, animated: true, completion: nil)
+    }
+    
+    @objc func save() {
+        
+    }
+    
+    @objc func close() {
+        self.navigationController.dismiss(animated: true, completion: nil)
     }
 }
 
